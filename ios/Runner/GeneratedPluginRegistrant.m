3fdc708752b60888e6d3c5b39d27474b968a1f80
md-shihab-mia <business.shihab@gmail.com>
2025-07-22 21:23:29 +0600

    flutter ecommerce done

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,42 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
+#import <firebase_core/FLTFirebaseCorePlugin.h>
+#else
+@import firebase_core;
+#endif
+
+#if __has_include(<firebase_crashlytics/FLTFirebaseCrashlyticsPlugin.h>)
+#import <firebase_crashlytics/FLTFirebaseCrashlyticsPlugin.h>
+#else
+@import firebase_crashlytics;
+#endif
+
+#if __has_include(<flutter_sslcommerz/FlutterSslcommerzPlugin.h>)
+#import <flutter_sslcommerz/FlutterSslcommerzPlugin.h>
+#else
+@import flutter_sslcommerz;
+#endif
+
+#if __has_include(<shared_preferences_foundation/SharedPreferencesPlugin.h>)
+#import <shared_preferences_foundation/SharedPreferencesPlugin.h>
+#else
+@import shared_preferences_foundation;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [FLTFirebaseCrashlyticsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCrashlyticsPlugin"]];
+  [FlutterSslcommerzPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterSslcommerzPlugin"]];
+  [SharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"SharedPreferencesPlugin"]];
+}
+
+@end
---
+++ lib/env.dart
@@ -0,0 +1,10 @@
+class Env{
+  static final String sslStoreId = 'maynu687c572a7c5cd';
+  static final String sslStorePassword = 'maynu687c572a7c5cd@ssl';
+  static final String marchentUrl = 'https://sandbox.sslcommerz.com/manage/';
+  static final String storeName = 'testmaynu51mv';
+  static final String registrationUrl = 'www.maynul4.com';
+  static final String sessionApiToGenerateTransection = 'https://sandbox.sslcommerz.com/gwprocess/v3/api.php';
+  static final String validationApi = 'https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php?wsdl';
+  static final String validationApiWebService = 'https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php';
+}
